da92885440116f8e2daf44b00e2517c4db49a28b
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-08-05 14:24:02 -0500

    feat: add complete and nacimiento

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ import { ApolloServer, gql } from 'apollo-server'
 
 const persons = [
   {
-    id: 'cdcd-cddc-cdcc-cdaa',
-    name: 'John',
-    age: 29,
+    id: '1',
+    name: 'Jaen',
+    lastname: 'Figueroa',
+    age: 23,
     country: 'Peru',
     phone: '555-555-5555',
   },
   {
-    id: 'sacd-sfsd-dsgs-gsdg',
-    name: 'Jane',
+    id: '2',
+    name: 'Ahinara',
+    lastname: 'Victorazzi',
     age: 21,
     country: 'USA',
     phone: '555-555-5555',
   },
   {
-    id: 'sdsc-sdcs-dscs-dscc',
-    name: 'Bob',
+    id: '3',
+    name: 'Foxed',
+    lastname: 'Tailer',
     age: 25,
     country: 'Canada',
     phone: '555-555-5555',
@@ -29,27 +32,39 @@ const typeDefs = gql`
   type Person {
     id: ID!
     name: String!
+    lastname: String!
     age: Int!
     country: String!
     phone: String!
+
+    complete: String!
+    nacimiento: Int!
   }
 
   type Query {
     personCount: Int!
     allPersons: [Person]!
+    person(name: String!): Person
   }
 `
-// person(id: ID!): Person!
 
 /* DEFINIR COMO SE VAN A RESOLVER LO QUE SE PIDA */
 const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
-    // person: (root, args) => {
-    //   const person = persons.find(p => p.id === args.id)
-    //   return person
-    // },
+    person: (root, args) => {
+      const person = persons.find((p) => p.name === args.name)
+      return person
+    },
+  },
+
+  /* agregado para Person */
+  Person: {
+    complete: (root) => `${root.name} ${root.lastname}`,
+    nacimiento: (root) => {
+      return new Date().getFullYear() - root.age
+    },
   },
 }
 
